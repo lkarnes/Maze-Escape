1673af329bc75cef6c6583e93078d108a1c62bf6
Logan Karnes <logan.karnes98@gmail.com>
2025-01-23 21:38:17 -0800

    added player movement

--- scenes/Character/character.gd
+++ scenes/Character/character.gd
@@ -1,1 +1,18 @@
 extends CharacterBody2D
+
+var player_speed: int = 200;
+@onready var animations: AnimationPlayer = %AnimationPlayer;
+
+func _physics_process(delta):
+	handle_movement();
+
+func handle_movement():
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down");
+	velocity = direction * player_speed;
+	if direction == Vector2.ZERO:
+		animations.play('idle');
+	elif (direction.x > 0):
+		animations.play('run_right');
+	else:
+		animations.play('run_left');
+	move_and_slide();
---
+++ scripts/maze/maze.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func generate_maze():
+	pass;
