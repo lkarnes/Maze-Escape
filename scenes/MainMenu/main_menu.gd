9d1a0d85dbc753c218e1cb4f33b0245099f84b70
Nathan <xtinked97@gmail.com>
2025-01-30 21:57:37 -0800

    addding main menu

--- scenes/MainMenu/main_menu.gd
+++ scenes/MainMenu/main_menu.gd
@@ -1,23 +1,26 @@
 extends Control
 
+@onready var lobby_id = $LobbyID
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"VBoxContainer/StartGame".grab_focus()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_start_game_pressed():
-	get_tree().change_scene_to_file("res://scenes/World/World.tscn")
-
-
-func _on_options_pressed():
 	pass # Replace with function body.
 
 
-func _on_quit_pressed():
+func _process(delta: float) -> void:
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/World/World.tscn")
+
+func _on_host_button_pressed():
+	NetworkImpl.create_lobby()
+
+func _on_join_button_pressed():
+	var id: int = int(lobby_id.text)
+	NetworkImpl.join_lobby(id)
+
+
+func _on_exit_button_pressed():
 	get_tree().quit()
