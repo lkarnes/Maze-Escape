e7a5da53c4f034eb3c65f550bb1ad5c6813aae54
Christopher Cruz <christopherc1331@gmail.com>
2025-02-10 23:35:40 -0600

    refactor to use steam client management instead of enet

--- scripts/multiplayer/networks/network_manager.gd
+++ scripts/multiplayer/networks/network_manager.gd
@@ -3,16 +3,25 @@ extends Node
 @export var _players_spawn_node: Node2D
 
 var lobby_scene := preload('res://scenes/Lobby/Lobby.tscn')
-var lobby_initialized
+var lobby
 
 func _build_multiplayer_network():
-	pass
+	if not lobby:
+		print('Building network...')
+		lobby = lobby_scene.instantiate()
+		lobby._players_spawn_node = _players_spawn_node
+		add_child(lobby)
+		MultiplayerManager.multiplayer_mode_enabled = true
+	
 
-func _set_active_network():
-	lobby_initialized = lobby_scene.instantiate()
-	lobby_initialized._players_spawn_node = _players_spawn_node
-	add_child(lobby_initialized)
+func become_host():
+	_build_multiplayer_network()
+	SteamNetwork.become_host()
 
+func join_as_client(lobby_id = 0):
+	_build_multiplayer_network()
+	SteamNetwork.join_as_client(lobby_id)
+	
 func list_lobbies():
 	_build_multiplayer_network()
 	SteamNetwork.list_lobbies()
--- scripts/multiplayer/networks/steam_network.gd
+++ scripts/multiplayer/networks/steam_network.gd
@@ -5,28 +5,45 @@ var multiplayer_peer: SteamMultiplayerPeer = SteamMultiplayerPeer.new()
 var _players_spawn_node
 var _hosted_lobby_id = 0
 
-const LOBBY_NAME = "Maze Tank"
+const LOBBY_NAME = "Maze Escape"
 
 func _ready():
-	multiplayer_peer.lobby_created.connect(_on_lobby_created)
+	Steam.lobby_created.connect(_on_lobby_created.bind())
 
 func become_host():
 	print('Starting host!')
 	
-	multiplayer_peer.create_lobby(Steam.LOBBY_TYPE_PUBLIC, SteamManager.lobby_max_members)
-	multiplayer.multiplayer_peer = multiplayer_peer
 	multiplayer.peer_connected.connect(_add_player_to_game)
 	multiplayer.peer_disconnected.connect(_del_player)
 	
-	if not OS.has_feature('dedicated_server'):
-		_add_player_to_game(1)
+	Steam.lobby_joined.connect(_on_lobby_joined.bind())
+	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, SteamManager.lobby_max_members)
+	
 
 func join_as_client(lobby_id):
 	print('Joining lobby %s' % lobby_id)
 	
-	multiplayer_peer.connect_lobby(lobby_id)
-	multiplayer.multiplayer_peer = multiplayer_peer
-
+	Steam.lobby_joined.connect(_on_lobby_joined.bind())
+	Steam.joinLobby(int(lobby_id))
+	
+func _on_lobby_joined(lobby: int, permissions: int, locked: bool, response: int):
+	print('On lobby joined')
+	if response == 1:
+		var id = Steam.getLobbyOwner(lobby)
+		if id != Steam.getSteamID():
+			print('Connecting client to socket...')
+			connect_socket(id) 
+	else:
+		pass
+		
+func connect_socket(steam_id: int):
+	var res = multiplayer_peer.create_client(steam_id, 0)
+	if res == OK:
+		print('Connecting peer to host...')
+		multiplayer.set_multiplayer_peer(multiplayer_peer)
+	else:
+		print('Error creating client: %s' % str(res))
+		
 func _on_lobby_created(connect: int, lobby_id):
 	if connect == 1:
 		_hosted_lobby_id = lobby_id
@@ -34,12 +51,25 @@ func _on_lobby_created(connect: int, lobby_id):
 		
 		Steam.setLobbyJoinable(_hosted_lobby_id, true)
 		Steam.setLobbyData(_hosted_lobby_id, "name", LOBBY_NAME)
-
+		
+		_create_host()
+		
+func _create_host():
+	print('Creating host...')
+	var res = multiplayer_peer.create_host(0)
+	if res == OK:
+		multiplayer.set_multiplayer_peer(multiplayer_peer)
+		
+		if not OS.has_feature('dedicated_server'):
+			_add_player_to_game(1)
+		else:
+			print('Error creating host: %s' % str(res))
+			
 func list_lobbies():
 	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
 	# NOTE: If you are using the test app id, you will need to apply a filter on your game name
 	# Otherwise, it may not show up in the lobby list of your clients
-	Steam.addRequestLobbyListStringFilter("name", "Maze Tank", Steam.LOBBY_COMPARISON_EQUAL)
+	Steam.addRequestLobbyListStringFilter("name", LOBBY_NAME, Steam.LOBBY_COMPARISON_EQUAL)
 	Steam.requestLobbyList()
 
 func _add_player_to_game(id: int):
