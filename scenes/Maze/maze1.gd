534a1a97140f1cd129da14bf39b01dd8834e70e8
Logan Karnes <logan.karnes98@gmail.com>
2025-02-12 21:46:34 -0800

    WIP maze generation, lighting and walls

--- scenes/Maze/maze1.gd
+++ scenes/Maze/maze1.gd
@@ -1,22 +1,49 @@
 extends Node2D
 
-@export var maze_height: int = 16;
-@export var maze_width: int = 16;
+@export var maze_height: int = 80;
+@export var maze_width: int = 80;
 @onready var grass: TileMapLayer = %Grass;
+@onready var walls: TileMapLayer = %Walls;
 var maze_arr;
 func _ready():
 	# make sure the size is divisible by 4
 	if !maze_arr:
-		maze_arr = Maze.generate_maze(maze_width, maze_height);
-		print('maze 1:', maze_arr)
+		#maze_arr = Maze.generate_maze(maze_width, maze_height);
+		#print('maze 1:', maze_arr)
 		maze_arr = Maze.generate_maze_2(maze_width, maze_height);
 		print('maze 2:', maze_arr)
+		
+		for y in range(maze_height * 3):
+			for x in range(maze_width * 3):
+					grass.set_cell(Vector2i(x,y), 0, Vector2i(1,1))
+		
 		for y in range(maze_arr.size()):
 			for x in range(maze_arr[y].size()):
-				if maze_arr[y][x] == 0:
-					grass.set_cell(Vector2i(x,y), 0, Vector2i(1,1))
-				else:
-					grass.set_cell(Vector2i(x,y), 0, Vector2i(3,2))
+				
+				if maze_arr[y][x] == 1:
+					grass.set_cell(Vector2i(x * 3,y * 3), 0, Vector2i(3,2))
+					grass.set_cell(Vector2i(x * 3,y * 3), 0, Vector2i(0,0))
+					walls.set_cell(Vector2i(x * 3,y * 3), 0, Vector2i(0,0))
+					
+					# fill y gaps
+					if y > 1 and maze_arr[y - 1][x] == 1:
+						grass.set_cell(Vector2i(x * 3,(y * 3) - 1), 0, Vector2i(3,2))
+						grass.set_cell(Vector2i(x * 3,(y * 3) - 2), 0, Vector2i(3,2))
+						walls.set_cell(Vector2i(x * 3,(y * 3) - 1), 0, Vector2i(0,0))
+						walls.set_cell(Vector2i(x * 3,(y * 3) - 2), 0, Vector2i(0,0))
+					if y + 1 < maze_arr.size() and maze_arr[y + 1][x] == 1:
+						grass.set_cell(Vector2i(x * 3,(y * 3) + 1), 0, Vector2i(3,2))
+						grass.set_cell(Vector2i(x * 3,(y * 3) + 2), 0, Vector2i(3,2))
+#					# fill x gaps
+					if x + 1 < maze_arr[y].size() and maze_arr[y][x + 1] == 1:
+						grass.set_cell(Vector2i((x * 3) + 1,y * 3), 0, Vector2i(3,2))
+						grass.set_cell(Vector2i((x * 3) + 2,y * 3), 0, Vector2i(3,2))
+						walls.set_cell(Vector2i((x * 3) + 1,y * 3), 0, Vector2i(0,0))
+						walls.set_cell(Vector2i((x * 3) + 2,y * 3), 0, Vector2i(0,0))
+					if x > 0 and maze_arr[y][x - 1] == 1:
+						grass.set_cell(Vector2i((x * 3 - 1),y * 3), 0, Vector2i(3,2))
+						grass.set_cell(Vector2i((x * 3 - 2),y * 3), 0, Vector2i(3,2))
+						
 					
 func find_walkable_position() -> Vector2i:
 	var position: Vector2i;
@@ -31,7 +58,7 @@ func find_walkable_position() -> Vector2i:
 		
 		if maze_arr[y][x] == 0:
 			found_valid_position = true;
-			position = to_global(Vector2i(x * 16,y * 16))
+			position = to_global(Vector2i(x * 16 * 3,y * 16 * 3))
 	return position;
 	
 	
