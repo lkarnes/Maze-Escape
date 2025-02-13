db23ff2b9693040690f7b5da75137bf4f417a395
Christopher Cruz <christopherc1331@gmail.com>
2025-02-13 01:22:35 -0600

    refactor to expose player spawner

--- scenes/World/world.gd
+++ scenes/World/world.gd
@@ -7,8 +7,11 @@ var player_spawned = false;
 func _physics_process(delta: float) -> void:
 	if !player_spawned:
 		player_spawned = true;
+		spawn_players([true])
+		
+	
+func spawn_players(players):
+	for _player in players:
 		var player = PLAYER.instantiate();
 		player.global_position = maze.find_walkable_position();
 		add_child(player);
-		
-	
