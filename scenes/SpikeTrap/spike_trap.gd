e699461f2792504fd8d7d4ecb91ac49b7600c10e
Logan Karnes <logan.karnes98@gmail.com>
2025-02-25 21:27:13 -0800

    added spike trap

--- scenes/SpikeTrap/spike_trap.gd
+++ scenes/SpikeTrap/spike_trap.gd
@@ -5,6 +5,7 @@ var trap_armed = false;
 var item_type = 'spike_trap';
 @onready var sprite: Sprite2D = %Sprite2D;
 @onready var animations: AnimationPlayer = %AnimationPlayer;
+@onready var trigger_noise: AudioStreamPlayer2D = %TriggerNoise;
 
 func pickup():
 	queue_free();
@@ -12,9 +13,11 @@ func pickup():
 func arm_trap() -> void:
 	sprite.frame = 1;
 	await get_tree().create_timer(1.0).timeout;
-	trap_armed = true
+	trap_armed = true;
 
 func _on_area_entered(area: Area2D) -> void:
 	if 'take_damage' in area and trap_armed:
 		animations.play('trigger');
+		trigger_noise.pitch_scale = randf_range(0.9, 1.1);
+		trigger_noise.play();
 		area.take_damage(1);
